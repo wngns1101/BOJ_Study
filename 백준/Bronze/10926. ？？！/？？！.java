316c653ee897e6b3ece8041d36c13d5d952ce1f9
이주훈 <86885227+wngns1101@users.noreply.github.com>
2022-06-16 10:16:51 +0900

    Update ？？！.java

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,11 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+//import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String a = br.readLine();
 		System.out.println(a + "??!");
+		//Scanner sc = new Scanner(System.in);
+		//String a = sc.nextLine();
+		//System.out.println(a + "??!");
+		//sc.close();
 	}
 }
