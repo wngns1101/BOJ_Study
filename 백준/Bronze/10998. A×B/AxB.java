4dc148f18869e4c32fa8a6353bccce76303b108b
이주훈 <86885227+wngns1101@users.noreply.github.com>
2022-06-17 09:24:22 +0900

    Create AxB.java

---
+++ 백준/Bronze/10998. A×B/AxB.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String a = br.readLine();
+		String[] b = a.split(" ");
+		int no1 = Integer.parseInt(b[0]);
+		int no2 = Integer.parseInt(b[1]);
+		System.out.println(no1 * no2);
+	}
+}
