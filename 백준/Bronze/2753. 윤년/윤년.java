54777f18551443855f89ea7476f4c17267790b4c
이주훈 <86885227+wngns1101@users.noreply.github.com>
2022-06-25 22:53:33 +0900

    [Bronze V] Title: 윤년, Time: 120 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -5,18 +5,11 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
-		int no1 = Integer.parseInt(str);
-		if (no1 % 4 == 0) {
-			if (no1 % 400 == 0) {
-				System.out.println("1");
-			}else if(no1 % 100 == 0) {
-				System.out.println("0");
-			}else {
-				System.out.println("1");
-			}
-		} else {
-			System.out.println("0");
+		int year = Integer.parseInt(br.readLine());
+		if(year%4==0 && year%100!=0 || year%400==0) {
+			System.out.println("1");
+		}else {
+			 System.out.println("0");
 		}
 	}
 }
