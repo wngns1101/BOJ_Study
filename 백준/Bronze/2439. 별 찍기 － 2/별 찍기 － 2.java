f362165dbc58d6d18be82ed24c2170ca935592da
이주훈 <86885227+wngns1101@users.noreply.github.com>
2022-07-03 01:30:41 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 244 ms, Memory: 16752 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,20 +1,19 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int index = Integer.parseInt(br.readLine());
-		for (int i = 1; i <= index; i++) {
-			for (int j = i; j < index; j++) {
-				System.out.print(" ");
-			}
-			for (int k = 0; k < i; k++) {
-				System.out.print("*");
-			}
+	   public static void main(String[] args) throws IOException {
+	      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-			System.out.println("");
-		}
+	      int N = Integer.parseInt(br.readLine());
+
+	      for (int i = 0; i < N; i++) {
+	         for (int j = 1; j < N - i; j++) {
+	            System.out.print(" ");
+	         }
+	         for(int t = 1; t <= i+1; t++ ) {
+	            System.out.print("*");
+	         }
+	         System.out.println();
+	      }
+	   }
 	}
-}
