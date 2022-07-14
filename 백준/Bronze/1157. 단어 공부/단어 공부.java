84b79d8f7d99d0087e788e412cfd141335dfb1d2
이주훈 <86885227+wngns1101@users.noreply.github.com>
2022-07-14 13:56:45 +0900

    [Bronze I] Title: 단어 공부, Time: 624 ms, Memory: 86760 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -3,38 +3,30 @@ import java.io.*;
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] str = br.readLine().split("");
-		String[] sletter = "abcdefghijklmnopqrstuvwxyz".split("");
-		String[] cletter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?".split("");
-		int[] num = new int[26];
-		String sum = null;
+		String[] str = br.readLine().toUpperCase().split("");
+		String[] cLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?".split("");
+		int word[] = new int[26];
 		int max = 0;
 		int index = 0;
-
 		for (int i = 0; i < str.length; i++) {
-			for (int j = 0; j < sletter.length; j++) {
-				if (str[i].equals(sletter[j]) || str[i].equals(cletter[j])) {
-					++num[j];
+			for (int j = 0; j < cLetter.length; j++) {
+				if (str[i].equals(cLetter[j])) {
+					++word[j];
 				}
 			}
 		}
 
-		for (int i = 0; i < num.length; i++) {
-			if (num[i] > 0) {
-				sum += "/";
-				sum += Integer.toString(i);
-			}
-		}
-		String[] slice = sum.split("/");
-		for (int i = 1; i < slice.length; i++) {
-			if (num[Integer.parseInt(slice[i])] > max) {
-				max = num[Integer.parseInt(slice[i])];
-				index = Integer.parseInt(slice[i]);
-			} else if (num[Integer.parseInt(slice[i])] == max) {
-				index = 26;
+		for (int i = 0; i < cLetter.length - 1; i++) {
+			if (word[i] > 0) {
+				if (word[i] > max) {
+					max = word[i];
+					index = i;
+				} else if (word[i] == max) {
+					index = 26;
+				}
 			}
 		}
 
-		System.out.println(cletter[index]);
+		System.out.println(cLetter[index]);
 	}
 }
