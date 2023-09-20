df9a8a52de00974b4284ce1490fb5bf9efa54c21
이주훈 <86885227+wngns1101@users.noreply.github.com>
2023-09-20 10:22:28 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,12 +1,4 @@
 pieces = [1, 1, 2, 2, 2, 8]
 board = list(map(int, input().split(" ")))
-result = ""
-for index, piece in enumerate(pieces):
-    if piece == board[index]:
-        result += "0 "
-    else:
-        if board[index] > piece:
-            result += str(-1*(board[index] - piece)) + " "
-        else:
-            result += str(piece - board[index]) + " "
-print(result.strip())
+for i in range(len(pieces)):
+    print(pieces[i]-board[i], end=" ")
