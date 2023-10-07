61f4b8641574cc5dca8df3504e18f3c7f29cf84a
이주훈 <86885227+wngns1101@users.noreply.github.com>
2023-10-07 12:08:23 +0900

    [Bronze III] Title: 약수 구하기, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,10 @@
+num1, num2 = map(int, input().split())
+count = 0
+for i in range(1, num1+1):
+    if num1 % i == 0:
+        count += 1
+    if count == num2:
+        print(i)
+        break
+if count < num2:
+    print(0)
