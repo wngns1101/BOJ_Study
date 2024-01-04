f45dcfbbfd145c0025ea384380c37595a7ee3abf
이주훈 <wngns0812@gmail.com>
2024-01-04 23:03:44 +0900

    [level unrated] Title: 공배수, Time: 0.01 ms, Memory: 61.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181936. 공배수/공배수.kt
+++ 프로그래머스/unrated/181936. 공배수/공배수.kt
@@ -1,9 +1,6 @@
 class Solution {
     fun solution(number: Int, n: Int, m: Int): Int {
-        if((number % n) == 0 && (number % m) == 0){
-            return 1
-        }else{
-            return 0
-        }
+    
+        return if ((number % n) == 0 && (number % m) == 0) 1 else 0
     }
 }
