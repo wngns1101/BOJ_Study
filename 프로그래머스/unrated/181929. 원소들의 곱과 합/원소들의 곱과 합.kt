a1b15525ef776be171113e80cf334e55fd7cdb81
이주훈 <wngns0812@gmail.com>
2024-01-07 17:40:46 +0900

    [level unrated] Title: 원소들의 곱과 합, Time: 15.59 ms, Memory: 64.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.kt
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.kt
@@ -2,10 +2,7 @@ import kotlin.math.pow
 class Solution {
     fun solution(num_list: IntArray): Int {
         var answer: Int = 0
-        var numListMul = 1
-        for (i in num_list){
-            numListMul = numListMul * i
-        }
+        var numListMul = num_list.reduce{total, num -> total * num}
         var numListSum = num_list.sum().toFloat().pow(2).toInt()
         
         if (numListMul < numListSum){
