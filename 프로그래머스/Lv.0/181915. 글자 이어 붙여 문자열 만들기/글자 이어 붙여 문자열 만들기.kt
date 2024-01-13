b9f4cdabf6f529d9dfacfae770c8ebbc027db8cd
이주훈 <wngns0812@gmail.com>
2024-01-13 13:22:36 +0900

    [level Lv.0] Title: 글자 이어 붙여 문자열 만들기, Time: 11.12 ms, Memory: 61.7 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(my_string: String, index_list: IntArray): String {
+        var answer: String = ""
+        for (i in index_list){
+            answer += my_string[i]
+        }
+        return answer
+    }
+}
