7fa2522a8f4dbef5c5664eeca0e878896a5039ca
이주훈 <wngns0812@gmail.com>
2024-01-15 21:38:01 +0900

    [level Lv.0] Title: 문자열 여러 번 뒤집기, Time: 39.98 ms, Memory: 65 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(my_string: String, queries: Array<IntArray>): String {
+        var answer: String = my_string
+        for (query in queries) {
+            val start = query[0]
+            val end = query[1].coerceAtMost(answer.length - 1) // Ensure end index is within bounds
+            val reversedSubstring = answer.substring(start, end + 1).reversed()
+            answer = answer.substring(0, start) + reversedSubstring + answer.substring(end + 1)
+        }
+        return answer
+    }
+}
