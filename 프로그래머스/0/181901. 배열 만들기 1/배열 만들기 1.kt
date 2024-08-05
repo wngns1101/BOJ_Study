123881954185f2d3bd2b5a39fe5d0162cb1750fd
이주훈 <wngns0812@gmail.com>
2024-08-05 10:00:41 +0900

    [level 0] Title: 배열 만들기 1, Time: 28.90 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(n: Int, k: Int): IntArray {
+        var answer: IntArray = intArrayOf()
+        
+        for(i in 1..n) {
+            if (i % k == 0) answer += i
+        }
+        
+        return answer
+    }
+}
