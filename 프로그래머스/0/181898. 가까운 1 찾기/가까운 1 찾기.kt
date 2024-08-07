23b8e1267538fcb6d65e1d1a439bb5f8f943e0e9
이주훈 <wngns0812@gmail.com>
2024-08-07 09:51:02 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 63.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun solution(arr: IntArray, idx: Int): Int {
+        var answer: Int = -1
+        
+        for (i in idx..arr.size-1) {
+            if(arr[i] == 1) {
+                answer = i
+                break
+            }
+        }
+    
+        return answer
+    }
+}
